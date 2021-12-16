e713b78eb8aa0da932bf0f7900e46a199de80a4b
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2021-12-16 19:53:37 +0100

    added possible_codes

--- src/utils.c
+++ src/utils.c
@@ -7,3 +7,10 @@ int compare_string(const char *s1, const char *s2){
 		s1++;
 	return *s1 - *s2;
 }
+
+double possible_codes(const double number_of_colors, const double code_length)
+/* returns the possible number of combinations */
+{
+    double c = pow(number_of_colors, code_length);
+    return c;
+}
