90f46365063a85b617276cc7ffc78002e5b27e98
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2021-12-16 19:54:28 +0100

    added possible_codes

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H_INCLUDE
 #define UTILS_H_INCLUDE
 
+#include <math.h>
+
+double possible_codes(const double number_of_colors, const double code_length);
 int compare_string(const char *s1, const char *s2);
 
 #endif
