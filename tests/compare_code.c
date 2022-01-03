6f7b0974a5fb123ebd3a98de593c55d779874898
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-01-03 23:30:41 +0100

    final

--- tests/compare_code.c
+++ tests/compare_code.c
@@ -1,8 +1,3 @@
-/* 
-BUG YYBB GBRV 0 2 should be 0 1
-TODO use set_pebles function
-TODO Access most recent GUESS
-TODO remove main and set_pebles function (included for testing purposes) */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,20 +6,7 @@ TODO remove main and set_pebles function (included for testing purposes) */
 
 #define USED_A 'q' /* this char indicates, that this peg has been accounted for during comparison for black pegs */
 #define USED_B 'a' /* this char indicates, that this peg has been accounted for during comparison for white pegs two different are necessary as otherwise false positives will be indicated */
-int compare_code(row_t *row);
 
-void set_pebles(row, count_correct, count_color_correct) {
-    printf("row: %i, Black Pegs: %i, White Pegs: %i\n", row, count_correct, count_color_correct);
-}
-
-int main()
-{
-    char code[] = "GBRV";
-    char guess[] = "RRVY";
-    compare_code(guess, code);
-
-    printf("Guess: %s\nKey: %s\n", guess, code);
-}
 
 int compare_code(row_t *row)
 /* Function which compares the guess string with the secret code string and determines the number of black pegs (correct char and index) and white pegs (correct char , wrong index).
@@ -65,7 +47,6 @@ Returns: 0 if everything went fine. */
             }
         }
     }
-    int row = 1; /* FOR TESTING - REMOVE BEFORE RELEASE */
     set_pebles(row, count_correct, count_color_correct);
     return 0;
 }
